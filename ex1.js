736e11be361f054e441a3331ed576beb3df0e8e3
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-05-13 15:13:00 +0700

    exercise1

---
+++ ex1.js
@@ -0,0 +1,19 @@
+let dayNumber = 99;
+
+if (dayNumber === 0) {
+    console.log("Sunday");
+} else if (dayNumber === 1) {
+    console.log("Monday");
+} else if (dayNumber === 2) {
+    console.log("Tuesday");
+} else if (dayNumber === 3) {
+    console.log("Wednesday");
+} else if (dayNumber === 4) {
+    console.log("Thursday");
+} else if (dayNumber === 5) {
+    console.log("Friday");
+} else if (dayNumber === 6) {
+    console.log("Saturday");
+} else {
+    console.log("There are only seven days");
+}
